46cb4a8ccb67c6b2baff31bc776b4e3a93f6981f
alekseiBoiko <mrboikoaleksei@gmail.com>
2021-08-18 18:28:51 +0300

    creation header+welcome

---
+++ js/functions.js
@@ -0,0 +1,33 @@
+$(function(){
+  sortIsotope();
+  }
+);
+
+function sortIsotope() {
+  
+  var $list = $('.works-list').isotope({
+    itemSelector: '.works-item',
+    percentPosition: true,
+    layoutMode: 'fitRows'
+  });
+  
+  $list.imagesLoaded().progress(function(){
+    $list.isotope('layout');
+  });
+  
+  $('.filter-list').on( 'click', 'button', function() {
+  var filterValue = $( this ).attr('data-filter');
+  // use filterFn if matches value
+  // filterValue = filterFns[ filterValue ] || filterValue;
+  $('.works-list').isotope({ filter: filterValue });
+  });
+  
+  // change is-checked class on buttons
+  $('.filter-list').each( function( i, buttonGroup ) {
+    var $buttonGroup = $( buttonGroup );
+    $buttonGroup.on( 'click', 'button', function() {
+      $buttonGroup.find('.filter-active').removeClass('filter-active');
+      $( this ).addClass('filter-active');
+    });
+  });
+};
