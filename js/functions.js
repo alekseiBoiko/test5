bc3100f65f30cebb4c2e38c352f7c679c08af182
alekseiBoiko <mrboikoaleksei@gmail.com>
2021-08-20 18:19:58 +0300

    start About

--- js/functions.js
+++ js/functions.js
@@ -5,24 +5,28 @@ $(function(){
 
 function sortIsotope() {
 
-  var responsiveIsotope = [ [480, 4] , [720, 6] ];
-  var itemsPerPageDefault = 8;
-  var itemsPerPage = defineItemsPerPage();
-  var currentNumberPages = 1;
-  var currentPage = 1;
-  var pageAttribute = 'data-page';
-  var pagerClass = 'isotope-pager';
-  var currentFilter = '*';
-  var filterAttribute = 'data-filter';
-  var filterValue = "";
-  var $checkboxes = $('.filter-item');
+  // var itemSelector: '.works-item';
 
-  
   var $list = $('.works-list').isotope({
     itemSelector: '.works-item',
     percentPosition: true,
     layoutMode: 'fitRows'
   });
+  var $checkboxes = $('.filter-item');
+
+
+  var responsiveIsotope = [ [480, 4] , [720, 6] ];
+  var itemsPerPageDefault = 8;
+  var itemsPerPage = defineItemsPerPage();
+  var currentNumberPages = 1;
+  var currentPage = 1;
+  var currentFilter = '*';
+  var filterAttribute = 'data-filter';
+  var filterValue = "";
+  var pageAttribute = 'data-page';
+  var pagerClass = 'isotope-pager';
+
+  
   
   $list.imagesLoaded().progress(function(){
     $list.isotope('layout');
@@ -35,6 +39,7 @@ function sortIsotope() {
   $('.works-list').isotope({ filter: filterValue });
   });
   
+  
   // change is-checked class on buttons
   $('.filter-list').each( function( i, buttonGroup ) {
     var $buttonGroup = $( buttonGroup );
@@ -43,6 +48,29 @@ function sortIsotope() {
       $( this ).addClass('filter-active');
     });
   });
+  
+  function changeFilter(selector) { $list.isotope({ filter: selector }); }
+  
+  function goToPage(n) {
+    currentPage = n;
+    var selector = '.works-item';
+    var exclusives = [];
+        // for each box checked, add its value and push to array
+    $checkboxes.each(function (i, elem) {
+      if (elem.checked) {
+        selector += ( currentFilter != '*' ) ? '.'+elem.value : '';
+        exclusives.push(selector);
+      }
+    });
+        // smash all values back together for 'and' filtering
+    filterValue = exclusives.length ? exclusives.join('') : '*';
+        
+        // add page number to the string of filters
+    var wordPage = currentPage.toString();
+    filterValue += ('.'+wordPage);
+   
+    changeFilter(filterValue);
+  }
 
   function defineItemsPerPage() {
     var pages = itemsPerPageDefault;
@@ -59,48 +87,48 @@ function sortIsotope() {
   function setPagination() {
 
     var SettingsPagesOnItems = function(){
-    var itemsLength = $container.children(itemSelector).length;
-    var pages = Math.ceil(itemsLength / itemsPerPage);
-    var item = 1;
-    var page = 1;
-    var selector = itemSelector;
-    var exclusives = [];
+      var itemsLength = $list.children('.works-item').length;
+      var pages = Math.ceil(itemsLength / itemsPerPage);
+      var item = 1;
+      var page = 1;
+      var selector = '.works-item';
+      var exclusives = [];
 
             // for each box checked, add its value and push to array
-    $checkboxes.each(function (i, elem) {
-      if (elem.checked) {
-          selector += ( currentFilter != '*' ) ? '.'+elem.value : '';
-          exclusives.push(selector);
-      }
-    });      
+        $checkboxes.each(function (i, elem) {
+          if (elem.checked) {
+              selector += ( currentFilter != '*' ) ? '.'+elem.value : '';
+              exclusives.push(selector);
+          }
+        });      
             // smash all values back together for 'and' filtering
-    filterValue = exclusives.length ? exclusives.join('') : '*';
+        filterValue = exclusives.length ? exclusives.join('') : '*';
             // find each child element with current filter values
-            $container.children(filterValue).each(function(){
+        $list.children(filterValue).each(function(){
               // increment page if a new one is needed
-              if( item > itemsPerPage ) {
-                  page++;
-                  item = 1;
-              }
+          if( item > itemsPerPage ) {
+            page++;
+            item = 1;
+          }
               // add page number to element as a class
-              wordPage = page.toString();
+          wordPage = page.toString();
               
-              var classes = $(this).attr('class').split(' ');
-              var lastClass = classes[classes.length-1];
+          var classes = $(this).attr('class').split(' ');
+          var lastClass = classes[classes.length-1];
               // last class shorter than 4 will be a page number, if so, grab and replace
-              if(lastClass.length < 4){
-                  $(this).removeClass();
-                  classes.pop();
-                  classes.push(wordPage);
-                  classes = classes.join(' ');
-                  $(this).addClass(classes);
-              } else {
+          if(lastClass.length < 4){
+            $(this).removeClass();
+            classes.pop();
+            classes.push(wordPage);
+            classes = classes.join(' ');
+            $(this).addClass(classes);
+          } else {
                   // if there was no page number, add it
-                 $(this).addClass(wordPage); 
-              }
-              item++;
-          });
-        currentNumberPages = page;
+              $(this).addClass(wordPage); 
+          }
+          item++;
+        });    
+      currentNumberPages = page;
     }();
 
     // create page number navigation
@@ -111,17 +139,17 @@ function sortIsotope() {
         if(currentNumberPages > 1){
           for( var i = 0; i < currentNumberPages; i++ ) {
             var $pager = $('<a href="javascript:void(0);" class="pager" '+pageAttribute+'="'+(i+1)+'"></a>');
-            $pager.html(i+1);
+              $pager.html(i+1);
 
-            $pager.click(function(){
-              var page = $(this).eq(0).attr(pageAttribute);
-              goToPage(page);
-            });
+              $pager.click(function(){
+                var page = $(this).eq(0).attr(pageAttribute);
+                goToPage(page);
+              });
               
             $pager.appendTo($isotopePager);
           }
         }
-      $container.after($isotopePager);
+        $list.after($isotopePager);
     }();
   }
 };
